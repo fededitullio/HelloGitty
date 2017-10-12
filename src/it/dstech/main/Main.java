dfac736a8c029134000f5baa33242f3d4dbd28fb
ssorcinelli <32107841+ssorcinelli@users.noreply.github.com>
2017-10-12 17:20:41 +0200

    ADDED DATE TIME FUNCTION

--- src/it/dstech/main/Main.java
+++ src/it/dstech/main/Main.java
@@ -1,10 +1,15 @@
 package it.dstech.main;
 
+import java.time.*;
+
 public class Main {
 public static void main(String[] args) {
 	System.out.println("ciao a tutti  eeeh");
 	System.out.println("FUCK THE POLICE");
 	System.out.println("Finalmente edito anche io");
 	System.out.println("ho finto pure io di editare eeh!");
+	
+	LocalDateTime dataTime = LocalDateTime.now();
+	System.out.println(dataTime);
 }
 }
