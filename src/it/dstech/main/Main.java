c86d768c9872f9424a38b41d19dc81c32a3c3f45
fededitullio <feder.ditullio@gmail.com>
2017-10-12 16:19:38 +0200

    End of edit

--- src/it/dstech/main/Main.java
+++ src/it/dstech/main/Main.java
@@ -4,5 +4,7 @@ public class Main {
 public static void main(String[] args) {
 	System.out.println("ciao a tutti  eeeh");
 	System.out.println("FUCK THE POLICE");
-	System.out.prinln("Finalmente edito anche io");
+	System.out.println("Finalmente edito anche io");
+	System.out.println("ho finto pure io di editare eeh!");
+}
 }
